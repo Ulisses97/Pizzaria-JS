384909b6c1b3515577b2f42610c02a104e677e29
Ulisses Brandao <ulissespkk@gmail.com>
2020-12-27 19:34:50 -0300

    Colocando as info do JSON

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ pizzaJson.map( (item, index)=>{
   // Clonando meu HTML
   let pizzaItem = querySelector('.models .pizza-item').cloneNode(true);
 
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img
+
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
   // Preencher as informações em pizzaItem -> append pega o conteudo e add
   querySelector('.pizza-area').append(pizzaItem);
 
